821bc6912c502349f6ad00dbe78a820182d5e9c8
rashmishrm <rashmishrm74@gmail.com>
2017-08-08 16:18:45 -0700

    merge sorted arrays

--- Algo/src/com/practice/ccinterview/sortandsearch/MergeSortedArrays.java
+++ Algo/src/com/practice/ccinterview/sortandsearch/MergeSortedArrays.java
@@ -37,11 +37,32 @@ public class MergeSortedArrays {
 
 	}
 
+	public void mergefaster(int[] nums1, int m, int[] nums2, int n) {
+
+		int i1 = m - 1;
+		int i2 = n - 1;
+		int merged = m + n - 1;
+
+		while ( i2 >= 0) {
+			if (i1>=0 && nums1[i1] > nums2[i2]) {
+				nums1[merged] = nums1[i1];
+				i1--;
+			} else {
+				nums1[merged] = nums2[i2];
+				i2--;
+			}
+
+			merged--;
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 		int n[] = new int[] { 1, 2, 3, 0, 0, 0 };
 		int n1[] = new int[] { 2, 5, 6 };
 
-		new MergeSortedArrays().merge(n, 3, n1, 3);
+		new MergeSortedArrays().mergefaster(n, 3, n1, 3);
 
 		printArray(n);
 	}
