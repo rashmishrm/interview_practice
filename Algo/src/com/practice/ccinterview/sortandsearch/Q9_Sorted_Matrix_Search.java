1a7ce194708371b15863388a6925698f5e7950d5
rashmishrm <rashmishrm74@gmail.com>
2017-08-09 00:46:47 -0700

    sorting

---
+++ Algo/src/com/practice/ccinterview/sortandsearch/Q9_Sorted_Matrix_Search.java
@@ -0,0 +1,30 @@
+package com.practice.ccinterview.sortandsearch;
+
+public class Q9_Sorted_Matrix_Search {
+
+	public boolean searchMatrix(int[][] matrix, int n) {
+		 if(matrix==null || matrix.length==0){
+		        return false;
+		    }
+		int row = 0;
+		int col = matrix[0].length - 1;
+		while (col >= 0 && row < matrix.length) {
+			if (matrix[row][col] == n) {
+				return true;
+			}
+			else if (matrix[row][col] > n) {
+				col--;
+			}
+			else if (matrix[row][col] < n) {
+				row++;
+			}
+
+		}
+		return false;
+
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Q9_Sorted_Matrix_Search().searchMatrix(new int[][]{{1}}, 0));
+	}
+}
