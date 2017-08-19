d0744e4e2fff1533fea30e3dd4328563103c82b2
rashmishrm <rashmishrm74@gmail.com>
2017-08-18 22:42:01 -0700

    practice

---
+++ Algo/src/com/practice/karumanchi/chapter19/Q4_Max_Subsequence.java
@@ -0,0 +1,33 @@
+package com.practice.karumanchi.chapter19;
+
+public class Q4_Max_Subsequence {
+
+	public static int max_sum(int a[]) {
+		int sum = 0;
+		int m[] = new int[a.length];
+
+		for (int i = 1; i < a.length; i++) {
+			if (m[i - 1] + a[i] > 0) {
+				m[i] = m[i - 1] + a[i];
+			} else {
+				m[i] = 0;
+			}
+		}
+
+		int msum = 0;
+		for (int i = 0; i < m.length; i++) {
+			sum = m[i];
+
+			if (msum < sum) {
+				msum = sum;
+			}
+		}
+
+		return msum;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(max_sum(new int[] { -2, 11, -4, 13, -5, 2 }));
+	}
+}
